82aff22c0363ed69cf91031fcb40f09b44842fcd
IslamAbdAlazez <islamaldomyate@gmail.com>
2019-02-28 14:32:11 +0200

    Initial commit

---
+++ WEBSites/src/main/java/AutomationJourney2019/WEBSites/Udemy/App.java
@@ -0,0 +1,13 @@
+package AutomationJourney2019.WEBSites.Udemy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
